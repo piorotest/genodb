069132775e497690198c3fd541361ee38dbdae72
robot <robot@icbf.com>
2025-08-14 14:37:31 +0100

    Revert "runner check 2025-08-14 13:36:54"
    This reverts commit b1676fd620b0fa021b2197dadec0852b33987955.

--- PIORO/PROCEDURE/TEST2.sql
+++ PIORO/PROCEDURE/TEST2.sql
@@ -3,7 +3,7 @@
 begin 
  null;
  -- test 2344441113333422222224443sssssswwwwww33333332134242424sssswwww
-dupa; --ssss sssss wwwwww
+ -- dupa; --ssss sssss wwwwww
  -- nowy code sssssssss wwwwww ssssssss
  -- nowszy code
 end;
