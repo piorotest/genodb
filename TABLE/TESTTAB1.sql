a5a70c851840f0d0740747c205d8e5f68b649f56
robot <robot@icbf.com>
2026-06-05 14:44:55 +0100

    adding full

--- TABLE/TESTTAB1.sql
+++ TABLE/TESTTAB1.sql
@@ -1,5 +1,5 @@
 create table testtab1 (
   id number,
   first_name varchar2(100),
-  last_name varchar2(200) -- test aaaaaaa iiiii
+  last_name varchar2(200) -- test aaaaaaa aaaaaaaaaa 
 ) tablespace users;
